086109926d2c327469c11a31d82b7f0fd07c55e5
tempCross <bt.will777@gmail.com>
2019-05-12 17:21:59 -0400

    second commit

---
+++ remove_page.php
@@ -0,0 +1,34 @@
+<?php
+	include("LIB_parse.php");
+	include("LIB_http.php");
+
+	$web_page = http_get($target="http://www.phila.gov", $referer="");
+	$open_tag = "<li";
+	$close_tag = "</li>";
+	$remove_array = parse_array($web_page['FILE'], $open_tag, $close_tag);
+    
+    # Remove each occurrence of each array element from string;
+    for($xx=0; $xx<count($remove_array); $xx++)
+        $web_page = str_replace($remove_array, "", $web_page);
+    
+    $open_tag = "<script";
+	$close_tag = "</script>";
+	$remove_array = parse_array($web_page['FILE'], $open_tag, $close_tag);
+    
+    # Remove each occurrence of each array element from string;
+    for($xx=0; $xx<count($remove_array); $xx++)
+        $web_page = str_replace($remove_array, "", $web_page);
+
+    $open_tag = "<div";
+	$close_tag = "</div>";
+	$remove_array = parse_array($web_page['FILE'], $open_tag, $close_tag);
+    
+    # Remove each occurrence of each array element from string;
+    for($xx=0; $xx<count($remove_array); $xx++)
+        $web_page = str_replace($remove_array, "", $web_page);
+    
+    echo $web_page['FILE'];
+   
+    
+
+?>
