38262bf8dc620b774e3fe1c2afa6ca97575c8829
tempCross <bt.will777@gmail.com>
2019-05-12 17:24:29 -0400

    second commit

---
+++ array_parser.php
@@ -0,0 +1,12 @@
+<?php
+	include("LIB_parse.php");
+	include("LIB_http.php");
+
+	$web_page = http_get($target="http://www.fbi.gov", $referer="");
+
+	$meta_tag_array = parse_array($web_page['FILE'], "<meta", ">");
+
+	for($xx=0; $xx<count($meta_tag_array); $xx++)
+		echo $meta_tag_array[$xx]. "\n";
+
+?>
