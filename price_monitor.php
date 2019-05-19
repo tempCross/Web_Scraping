f6418964e6d0b60377644b463382b566cfe9ca94
tempCross <bt.will777@gmail.com>
2019-05-19 00:31:03 -0400

    price monirot update

--- price_monitor.php
+++ price_monitor.php
@@ -37,6 +37,34 @@ for($xx=0; $xx<count($table_array); $xx++)
 								echo "FOUND: id_column=$id_column\n";
 								echo "FOUND: price_column=$price_column\n";
 								echo "FOUND: name_column=$name_column\n";
+								# Save the heading row for later
+
+								$heading_row = $table_row;
+							}
+						$ending_landmark = "Calculate";
+						if((stristr($product_row_array[$table_row], $ending_landmark)))
+							{
+								echo "PARSING COMPLETE!\n";
+								break;
+							}
+						# Parse product and price data
+						if(isset($heading_row) && $heading_row<$table_row)
+							{
+								$table_cell_array = parse_array($product_row_array[$table_row], "<td", "</td>");
+								$product_array[$product_count]['ID'] = strip_tags(trim($table_cell_array[$id_column]));
+								$product_array[$product_count]['NAME'] = strip_tags(trim($table_cell_array[$name_column]));
+								$product_array[$product_count]['PRICE'] = strip_tags(trim($table_cell_array[$price_column]));
+								$product_count++;
+								echo "PROCESSED: Item #$product_count\n";
+
+							}
+						# Display the collected data
+						for ($xx=0; $xx<count($product_array); $xx++)
+							{
+								echo "$xx. ";
+								echo "ID: ".$product_array[$xx]['ID'].", ";
+								echo "NAME: ".$product_array[$xx]['NAME'].", ";
+								echo "PRICE: ".$product_array[$xx]['PRICE']."\n";
 							}
 					}
 				}
